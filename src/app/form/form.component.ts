1496c63956d2f4436b1e4cadf07c29810a77eec6
Erick Lui <erickjin.lui@gmail.com>
2017-06-15 16:08:40 -0700

    sorted quotes by most votes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -13,12 +13,33 @@ export class FormComponent implements OnInit {
   constructor() { }
   voteUp(data){
     data.rating += 1;
+    this.quotes = this.bubbleSort(this.quotes);
   }
   voteDown(data){
     data.rating -= 1;
+    this.quotes = this.bubbleSort(this.quotes);
   }
   delete(data){
     this.quotes.splice((this.quotes.indexOf(data)), 1);
+  }
+  bubbleSort(arr){
+    let sorted = false;
+    let swapCount = 0;
+    let temp = 0;
+    while (!sorted) {
+      swapCount = 0;
+      for (let i = 0; i < arr.length-1; i++){
+        if (arr[i].rating < arr[i+1].rating){
+          temp = arr[i];
+          arr[i] = arr[i+1];
+          arr[i+1] = temp;
+        }
+      }
+      if (swapCount === 0){
+        sorted = true;
+      }
+    }
+    return arr;
   }
   onSubmit(form){
     this.quotes.push(this.quote);
